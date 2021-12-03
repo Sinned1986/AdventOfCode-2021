c3986117f99cfcce73b8d773914da4da09de3e12
Dennis Blauhut <dennis.blauhut@dermalog.com>
2021-12-03 03:21:47 +0100

    solved day02a

--- adventofcode/puzzle/day01.py
+++ adventofcode/puzzle/day01.py
@@ -16,25 +16,26 @@ def day01a():
     incr = 0
     for op in diff_ops:
         if op[0] > op[1]:
-            incr = incr+1
-
+            incr = incr + 1
 
     return incr
 
+
 def day01b():
     measurements = numpy.array(read_measurements())
-    l = len(measurements) # raw measurements
-    w = 3 # filter window
-    filtered_measurement = measurements[0:l-(w-1)] + measurements[1:l-(w-2)] + measurements[2:l-(w-3)]
+    l = len(measurements)  # raw measurements
+    w = 3  # filter window
+    filtered_measurement = measurements[0:l - (w - 1)] + measurements[1:l - (w - 2)] + measurements[2:l - (w - 3)]
 
     l = len(filtered_measurement)
     incr = 0
-    diff = filtered_measurement[1:l] - filtered_measurement[0:l-1]
+    diff = filtered_measurement[1:l] - filtered_measurement[0:l - 1]
     for x in diff:
         if x > 0:
             incr = incr + 1
 
     return incr
+
 
 if __name__ == "__main__":
     print('%d measurements are larger then the previous ones' % day01a())
---
+++ adventofcode/puzzle/day02.py
@@ -0,0 +1,28 @@
+def read_movements():
+    movements = []
+    with open('day/2/input.txt') as fv:
+        for row in fv:
+            items = row.split()
+            movements.append([items[0], int(items[1])])
+    return movements
+
+
+def day02a():
+    movements = read_movements()
+    h_pos = 0
+    depth = 0
+    down = 0;
+    up = 0;
+    for movement in movements:
+        if 'forward' == movement[0]:
+            h_pos = h_pos + movement[1]
+        elif 'up' == movement[0]:
+            depth = depth - movement[1]
+        elif 'down' == movement[0]:
+            depth = depth + movement[1]
+
+    return h_pos * depth
+
+
+if __name__ == '__main__':
+    print('%d is the multiplication of the final horizontal position by the final depth' % (day02a()))
