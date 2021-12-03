39d193f4beb0ccf76b6c8a6068914a4e7fa300be
Dennis Blauhut <dennis.blauhut@dermalog.com>
2021-12-03 03:30:15 +0100

    solved day02b

--- adventofcode/puzzle/day02.py
+++ adventofcode/puzzle/day02.py
@@ -24,5 +24,24 @@ def day02a():
     return h_pos * depth
 
 
+def day02b():
+    movements = read_movements()
+    h_pos = 0
+    depth = 0
+    aim = 0
+
+    for movement in movements:
+        if 'forward' == movement[0]:
+            h_pos = h_pos + movement[1]
+            depth = depth + aim * movement[1]
+        elif 'up' == movement[0]:
+            aim = aim - movement[1]
+        elif 'down' == movement[0]:
+            aim = aim + movement[1]
+
+    return h_pos * depth
+
+
 if __name__ == '__main__':
     print('%d is the multiplication of the final horizontal position by the final depth' % (day02a()))
+    print('%d is the multiplication of the final horizontal position by the final depth' % (day02b()))
--- adventofcode/test/__init__.py
+++ adventofcode/test/__init__.py
@@ -1,7 +1,7 @@
 import unittest
 
 import adventofcode.puzzle.day01
-
+import adventofcode.puzzle.day02
 
 class RegressionTest(unittest.TestCase):
 
@@ -10,6 +10,12 @@ class RegressionTest(unittest.TestCase):
 
     def test_day1_b(self):
         self.assertEqual(adventofcode.puzzle.day01.day01b(), 1728)
+
+    def test_day2_a(self):
+        self.assertEqual(adventofcode.puzzle.day02.day02a(), 1813801)
+
+    def test_day2_b(self):
+        self.assertEqual(adventofcode.puzzle.day02.day02b(), 1960569556)
 
 
 if __name__ == '__main__':
