0ede3507d31c0ec59582fbd7575a8d6c25c9d661
Dennis Blauhut <dennis.blauhut@dermalog.com>
2021-12-05 01:52:33 +0100

    solved day04b

--- adventofcode/test.py
+++ adventofcode/test.py
@@ -20,14 +20,18 @@ class RegressionTest(unittest.TestCase):
     def test_day2_b(self):
         self.assertEqual(adventofcode.puzzle.day02.day02b(), 1960569556)
 
-    def test_day3_A(self):
+    def test_day3_a(self):
         self.assertEqual(adventofcode.puzzle.day03.day03a(), 3985686)
 
     def test_day3_b(self):
         self.assertEqual(adventofcode.puzzle.day03.day03b(), 2555739)
 
-    def test_day4_A(self):
+    def test_day4_a(self):
         self.assertEqual(adventofcode.puzzle.day04.day04a(), 65325)
+
+    def test_day4_b(self):
+        self.assertEqual(adventofcode.puzzle.day04.day04b(), 4624)
+
 
 
 if __name__ == '__main__':
