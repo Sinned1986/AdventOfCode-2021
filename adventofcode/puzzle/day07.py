b74a60a25f1431157a97f17b93963e393488f022
Dennis Blauhut <dennis.blauhut@dermalog.com>
2021-12-07 23:04:41 +0100

    solved day07b

--- adventofcode/puzzle/day07.py
+++ adventofcode/puzzle/day07.py
@@ -10,19 +10,36 @@ def read_pos():
     return np.array(positions)
 
 
-def day07a():
+def calc_linear_costs(x):
+    return x
+
+
+def calc_nonlinear_costs(x):
+    return np.array([sum(range(1, xi+1)) for xi in x])
+
+
+def calc_fuel(cost_func):
     positions = read_pos()
     min_pos = min(positions)
     max_pos = max(positions)
     least_fuel_required = sys.maxsize
     least_fuel_required_index = -1
     for i in range(min_pos, max_pos + 1):
-        fuel_required = np.sum(np.abs(positions - i))
+        fuel_required = np.sum(cost_func(np.abs(positions - i)))
         if fuel_required < least_fuel_required:
             least_fuel_required_index = i
             least_fuel_required = fuel_required
     return least_fuel_required
 
 
+def day07a():
+    return calc_fuel(calc_linear_costs)
+
+
+def day07b():
+    return calc_fuel(calc_nonlinear_costs)
+
+
 if __name__ == '__main__':
     print(day07a())
+    print(day07b())
