cd42a0de5eb8ad8fb92fec38c1505fca55afe30e
Dennis Blauhut <dennis.blauhut@dermalog.com>
2021-12-21 03:53:31 +0100

    cleanup

--- adventofcode/puzzle/day15.py
+++ adventofcode/puzzle/day15.py
@@ -1,8 +1,7 @@
 # Day 15: Chiton
 
 import numpy as np
-from enum import Enum
-import sys
+
 
 def read_map(file_name):
     cavern_map_rows = []
@@ -13,74 +12,7 @@ def read_map(file_name):
     return np.stack(cavern_map_rows)
 
 
-def add_border_to_cavern_map(cavern_map):
-    new_row = np.array([9]*len(cavern_map[0]))
-    new_column = np.array([[9], ]*(len(cavern_map)+2), )
-    tmp_array = np.vstack((new_row, cavern_map, new_row))
-    cavern_map_with_border = np.hstack((new_column, tmp_array, new_column))
-    return cavern_map_with_border
-
-
-class Prev(Enum):
-    NONE = 0
-    LEFT = 1
-    RIGHT = 2
-    TOP = 3
-    BOTTOM = 4
-    START = 5
-    END = 6
-
-
-def calc_cost(cavern_array, cost_array, prev_array, y, x):
-    cost = cost_array[y, x]
-    if y > 0:
-        if cavern_array[y - 1, x] + cost < cost_array[y - 1, x]:
-            cost_array[y - 1, x] = cavern_array[y - 1, x] + cost
-            prev_array[y - 1, x] = Prev.BOTTOM.value
-            calc_cost(cavern_array, cost_array, prev_array, y - 1, x)
-    if x > 0:
-        if cavern_array[y, x - 1] + cost < cost_array[y, x - 1]:
-            cost_array[y, x - 1] = cavern_array[y, x - 1] + cost
-            prev_array[y, x - 1] = Prev.RIGHT.value
-            calc_cost(cavern_array, cost_array, prev_array, y, x - 1)
-    if y < len(cavern_array) - 1:
-        if cavern_array[y + 1, x] + cost < cost_array[y + 1, x]:
-            cost_array[y + 1, x] = cavern_array[y + 1, x] + cost
-            prev_array[y + 1, x] = Prev.TOP.value
-            calc_cost(cavern_array, cost_array, prev_array, y + 1, x)
-    if x < len(cavern_array[0]) - 1:
-        if cavern_array[y, x + 1] + cost < cost_array[y, x + 1]:
-            cost_array[y, x + 1] = cavern_array[y, x + 1] + cost
-            prev_array[y, x + 1] = Prev.LEFT.value
-            calc_cost(cavern_array, cost_array, prev_array, y, x + 1)
-
-
-def mask_shortest_path(prev_array):
-    is_in_shortest_path_array = np.zeros(prev_array.shape, dtype=bool)
-    x = 0
-    y = 0
-    abort_cond = len(prev_array) * len(prev_array[0])
-    i = 0
-    while x != len(prev_array[0]) - 1 and y != len(prev_array) - 1:
-        is_in_shortest_path_array[y, x] = True
-        prev = prev_array[y, x]
-        if prev == Prev.BOTTOM.value:
-            y = y + 1
-        elif prev == Prev.RIGHT.value:
-            x = x + 1
-        elif prev == Prev.TOP.value:
-            y = y - 1
-        elif prev == Prev.LEFT.value:
-            x = x - 1
-        i += 1
-        if i > abort_cond:
-            raise Exception('invalid prev path')
-
-    return is_in_shortest_path_array
-
-
 def generate_map_for_part_2(cavern_array, h, v):
-    
 
     # extend array to te left
     tmp_array = cavern_array-1
@@ -162,8 +94,6 @@ def day15b(input_file_name):
 
     calced_costs = calc_dijkstra(cavern_array=cavern_array, start=[cavern_array.shape[0]-1, cavern_array.shape[1] - 1], target=[0, 0])
     return calced_costs
-
-
 
 
 if __name__ == '__main__':
